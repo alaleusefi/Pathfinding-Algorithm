f5cc66678dacb9649fa7f4b76135336cc4ae6322
L\Administrator <alaleusefi@yahoo.com>
2019-05-27 16:36:54 +0100

    Create seaMap based on the input

--- Pathfinding.js
+++ Pathfinding.js
@@ -1,10 +1,13 @@
 start();
 
 function start() {
-    pathStart = null;
-    pathEnd = null;
-    obstacles = [];
-    seaMap = { minX: 0, minY: 0, maxX: 0, maxY: 0 };
+    seaMap = {
+        pathStart: null,
+        pathEnd: null,
+        minX: 0, minY: 0, maxX: 0, maxY: 0,
+        reefs: [],
+        sea: []
+    };
 
     if (process.argv.length < 3) {
         console.log('Usage: node ' + "Pathfinding.js" + ' [#input-file#].txt');
@@ -23,31 +26,47 @@ function start() {
 function parseInput(inputSplit) {
     let validInputSplit = inputSplit.filter(s => s.isValidCoordinate());
 
-    pathStart = validInputSplit[0].parseToCoordinateAs("S");
-    pathEnd = validInputSplit[validInputSplit.length - 1].parseToCoordinateAs("E");
+    seaMap.pathStart = validInputSplit[0].parseToCoordinateAs("S");
+    seaMap.pathEnd = validInputSplit[validInputSplit.length - 1].parseToCoordinateAs("E");
 
     for (var i = 1; i <= validInputSplit.length - 2; i++) {
-        obstacles.push(validInputSplit[i].parseToCoordinateAs("X"));
+        seaMap.reefs.push(validInputSplit[i].parseToCoordinateAs("X"));
     }
     //Todo: See if the input makes sense in a broad definition
-    DetermineRange(obstacles);
+    DetermineRange();
+    FillMap();
 }
 
-DetermineRange = function (coords) {
+DetermineRange = function () {
     //Todo: Use Math.min like this:
     // minX = Math.min(coords.map(c => parseInt(c.X)));
     // maxX = Math.max(coords.map(c => parseInt(c.X)));
     // minY = Math.min(coords.map(c => parseInt(c.Y)));
     // maxY = Math.max(coords.map(c => parseInt(c.Y)));
-    coords.forEach(elem => {
+    seaMap.reefs.forEach(elem => {
         if (elem.X < seaMap.minX) seaMap.minX = elem.X;
         if (elem.X > seaMap.maxX) seaMap.maxX = elem.X;
         if (elem.Y < seaMap.minY) seaMap.minY = elem.Y;
         if (elem.Y > seaMap.maxY) seaMap.maxY = elem.Y;
     });
+}
+
+FillMap = function () {
+    for (var x = seaMap.minX; x <= seaMap.maxX; x++)
+        for (var y = seaMap.minY; y <= seaMap.maxY; y++) {
+            if (seaMap.reefs.find(c => c.X == x && c.Y == y) != undefined)
+                continue;
+            if (x == seaMap.pathStart.X && y == seaMap.pathStart.Y)
+                continue;
+            if (x == seaMap.pathEnd.X && y == seaMap.pathEnd.Y)
+                continue;
+            seaMap.sea.push(new Coordinate(x, y, "."));
+        }
 
     console.log(seaMap);
-}
+    console.log(seaMap.reefs.length);
+    console.log(seaMap.sea.length);
+};
 
 Coordinate = function (x, y, type) {
     this.X = x;
